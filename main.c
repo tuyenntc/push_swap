0b51e43e8fc80bda6faf547d024f1b4cbf235d35
tuyennguyen <tuyenntc@gmail.com>
2024-05-13 14:34:04 +0200

    fix input as string

--- main.c
+++ main.c
@@ -37,6 +37,7 @@ int main(int ac, char **av)
 		else
 			sort_stacks(&a, &b);
 	}
+//	free_str(av);
 	deallocate(&a);
 	deallocate(&b);
 	return (0);
@@ -45,20 +46,20 @@ int main(int ac, char **av)
 int main(int ac, char **av) {
     t_node  *a;
     t_node  *b;
+	char **split_av;
 
     a = NULL;
     b = NULL;
     if (ac == 1 || (ac == 2 && !av[1][0]))
         return (1);
     else if (ac == 2) {
-        char **split_av = split(av[1], ' ');
-        stack_init(&a, split_av + 1);
+        split_av = split(av[1], ' ');
+        stack_init(&a, split_av);
 
-        // Print values of split_av
-        printf("Values of split_av:\n");
-        for (int i = 0; split_av[i]; i++) {
-            printf("split_av[%d] = %s\n", i, split_av[i]);
-        }
+//        printf("Values of split_av:\n");
+//      for (int i = 0; split_av[i]; i++) {
+//            printf("split_av[%d] = %s\n", i, split_av[i]);
+//        }
 
         // Free split_av
         // free_str(split_av);
@@ -66,10 +67,10 @@ int main(int ac, char **av) {
         stack_init(&a, av + 1);
 
         // Print values of av
-        printf("Values of av:\n");
-        for (int i = 0; i < ac; i++) {
-            printf("av[%d] = %s\n", i, av[i]);
-        }
+//      printf("Values of av:\n");
+//      for (int i = 0; i < ac; i++) {
+//            printf("av[%d] = %s\n", i, av[i]);
+//        }
     }
 	if (!stack_sorted(a))
 	{
@@ -84,4 +85,3 @@ int main(int ac, char **av) {
 	deallocate(&b);
 	return (0);
 }
-
